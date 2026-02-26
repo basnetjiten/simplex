ed6cceca266a178b76794ba9944f0fa311623839
Jiten <jitenbasnet7@gmail.com>
2026-02-26 09:52:37 +0545

    revert changes

--- lib/base/simplex_base_remote_source.dart
+++ lib/base/simplex_base_remote_source.dart
@@ -92,7 +92,7 @@ class SimplexBaseRemoteSource {
     try {
       final OperationResponse<TData, TVars> response = await graphqlClient!
           .request(operationRequest)
-          .firstWhere((OperationResponse<TData, TVars> r) => !r.loading);
+          .first;
 
       SimplexAppLogger.logInfo(
         info: '🚀 API Request: ${operationRequest.operation.operationName}',
